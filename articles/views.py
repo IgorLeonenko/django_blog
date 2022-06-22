69a7c458ff07b9328916f41fe3308bffa5e300f7
Igor Leonenko <igrleon@gmail.com>
2022-06-22 21:09:34 +0200

    extend templates

--- articles/views.py
+++ articles/views.py
@@ -4,3 +4,7 @@ from .models import Article
 def index(request):
   articles = Article.objects.all().order_by('id')
   return render(request, 'index.html', {'articles': articles})
+
+def show(request, slug):
+  article = Article.objects.get(slug=slug)
+  return render(request, 'show.html', {'article': article})
--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
   path('articles/', include('articles.urls')),
@@ -8,3 +10,5 @@ urlpatterns = [
   path('about/', views.about),
   path('', views.home),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
